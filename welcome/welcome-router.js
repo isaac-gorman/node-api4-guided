f05bb7465ce01c80509f0ff13bdb10343885ca7d
Isaac Gorman <emailme@isaacg.net>
2020-09-28 15:36:55 -0500

    prog2

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -5,7 +5,9 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).json({
     message: `Welcome ${process.env.COHORT}`,
-    fun_fact: `Did you know that... ${process.env.FUN_FACT} || there are no muscles in your fingers`,
+    fun_fact:
+      `Did you know that... ${process.env.FUN_FACT}` ||
+      " there are no muscles in your fingers",
   });
 });
 
