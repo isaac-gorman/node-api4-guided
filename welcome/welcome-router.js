8b4c382d0ef46ac9e79b8e0bbebee18cfef02e52
Isaac Gorman <emailme@isaacg.net>
2020-09-28 15:32:29 -0500

    prog

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -1,11 +1,12 @@
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {
-	res.status(200).json({
-		message: "Welcome",
-	})
-})
+  res.status(200).json({
+    message: `Welcome ${process.env.COHORT}`,
+    fun_fact: `Did you know that... ${process.env.FUN_FACT} || there are no muscles in your fingers`,
+  });
+});
 
-module.exports = router
+module.exports = router;
